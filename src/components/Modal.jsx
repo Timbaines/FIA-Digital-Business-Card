8b4bea15e3087f459d218c6c3496f5eb6f9cbfca
Tim Baines <timbaines@timbaines.com>
2024-05-23 23:37:52 -0400

    Updated React Icons

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -41,7 +41,7 @@ const Modal = () => {
                                             className="modal-icon"/>Email</a>
                                     </div>
                                 </div>
-                                <p className="primary-font">Ready to begin your calling? If you have a heart to serve, we can find a job for you! You can download the forms that are available below.</p>
+                                <p className="primary-font">Ready to begin your calling? If you have a heart to serve, we can find a job for you! You can download the the available forms below.</p>
                                 <button className="btn btn-download"><AiOutlineCloudDownload className="download-icon"/>Volunteer
                                     App
                                 </button>
