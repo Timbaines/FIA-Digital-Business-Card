972448a9b2a0d99b1dac38d78ff9e20720ccabb4
Tim Baines <timbaines@timbaines.com>
2024-05-23 22:07:11 -0400

    Updated React Icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FiArrowUpRight } from 'react-icons/fi';
 
 export default function Card(props) {
 
     return (
         <div className="card">
-            <p className="card-number">{props["spot"]}</p>
+            <p className="card-number">{props.spot}</p>
             <img src={`${props.socialBadge}`} className="card-badge" alt="card-image"/>
             <button
                 className="btn-connect">{props.connect}
                 <span>Visit</span>
-                <FontAwesomeIcon icon={faArrowRight} />
+                <FiArrowUpRight className="arrow-icon" />
             </button>
         </div>
     )
